3ff0e440ee8845b7ecda8a2df1506a901466d73e
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-25 19:59:28 +0530

    refactor: Optimized imports

--- src/main/java/com/academicaffairs/odtrack/repository/UserRepository.java
+++ src/main/java/com/academicaffairs/odtrack/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.academicaffairs.odtrack.repository;
 
 import com.academicaffairs.odtrack.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
--- src/main/java/com/academicaffairs/odtrack/service/CustomUserDetailsService.java
+++ src/main/java/com/academicaffairs/odtrack/service/CustomUserDetailsService.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
